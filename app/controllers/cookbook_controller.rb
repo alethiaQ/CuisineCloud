0af6f78674944fc90409fc87b756bde7e94a361b
Alethia Quintero <alethiaq22@gmail.com>
2019-09-12 22:35:53 -0400

    User controller and views almost done. Cookbook controller/views updated

--- app/controllers/cookbook_controller.rb
+++ app/controllers/cookbook_controller.rb
@@ -1,2 +1,24 @@
 class CookBookController < ApplicationController
+  get "/cookbooks/new" do
+    erb :'/cookbooks/new' if logged_in?
+  end
+  get "/cookbooks/:id/edit" do
+    @book = CookBook.find_by(params[:id])
+    erb :'/cookbooks/edit'
+  end
+  post "/cookbooks/:id" do
+    @book = CookBook.find_by(params[:id])
+    #error message needed
+    @book.update(params)
+    redirect "/cookbooks/#{@book.id}"
+  end
+
+  get "/cookbooks/:id" do
+    @book = CookBook.find_by(params[:id])
+    erb :'/cookbooks/show'
+  end
+  post "/cookbooks/new" do
+    @book = CookBook.create(params) if logged_in?
+    redirect "/cookbooks/#{@book.id}"
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,58 @@
 class UserController < ApplicationController
+  get "/users/:id" do
+    if !logged_in?
+      redirect "/login"
+    end
+
+    @user = User.find(params[:id])
+    if !@user.nil? && @user == current_user
+      erb :'/users/accountpage'
+    else
+      redirect "/login"
+    end
+  end
+  get "/signup" do
+    if !session[:user_id]
+      erb :'/users/signup'
+    else
+      redirect "/"
+    end
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect "/signup"
+    else
+      @user = User.create(name: params[:name], username: params[:username], password: params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    end
+  end
+
+  get "/login" do
+    if !session[:user_id]
+      erb :'/users/login'
+    else
+      redirect "/"
+    end
+  end
+
+  post "/login" do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      redirect "/"
+    end
+  end
+
+  get "/logout" do
+    if session[:user_id] != nil
+      session.destroy
+      redirect "/"
+    else
+      redirect "/"
+    end
+  end
 end
